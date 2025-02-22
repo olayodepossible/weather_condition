594223741a6b21149be70b6fa6255d26b3a498b5
Akin <olayodepossible@gmail.com>
2025-02-22 13:52:50 +0100

    implement notFound view

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ def get_weather():
     weather_data = get_current_weather(city)
 
     # City is not found by API
-    if not weather_data['cod'] == 200:
+    if weather_data['cod'] != 200:
         return render_template('notFound.html')
 
     return render_template(
